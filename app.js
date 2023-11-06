474cde1049b0bd164b2ed19847fc454ed613add8
Petterson Grande <grande.petterson@gmail.com>
2023-11-06 12:57:35 -0500

    Umas unit 13.3 exercises

---
+++ app.js
@@ -0,0 +1,52 @@
+// exer 1
+
+// without arrowFunction
+
+// [2 , 3, 6, 78, 99, 104, 23].reduce(function(max, currNum) {
+//     return Math.max(max, currNum);
+// });
+
+// // with ArrowFunction
+
+// [2 , 3, 6, 78, 99, 104, 23].reduce((max,currNum) => {
+
+// return Math.max(max, currNum)
+
+// });
+
+// *************************
+// ARROW FUNCTIONS SHORTCUTS
+// *************************
+
+//  A single parameter
+
+// const greet = () => {
+//     console.log("Hello!")
+// };
+
+// greet()
+
+// IMPLICIT RETURN
+
+// [1,2,3,4,5,6,].filter(function(num){
+//     return num % 2 === 0;
+// });
+
+
+// [1,2,3,4,5,6,].filter((num) => num % 2 === 0);
+
+// const double = (n) => n * 2;
+
+//  or
+
+// const douBle = (n) => {
+
+//     return n * 2;
+
+// };
+
+
+
+
+
+
